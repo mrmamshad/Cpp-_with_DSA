c52c75e03843abc14fb2a17a8068a7bd8fa29988
Margubur Rahman Mamshad <mamshad3106@gmail.com>
2025-01-05 20:17:55 +0600

    Lecture 2 complete

---
+++ main.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    //sample code
+    //     int p, r, t;
+    // float si;
+
+    // cin >> p >> r >> t;
+// data type size 
+
+    // si = p * r * t / 100.0;
+
+    // cout << si << endl;
+    // cout << sizeof(si) << endl;
+
+// type casting  
+
+    // char grade = 'a';
+    // int value = grade;
+    // cout << value << endl;
+
+    // double price = 130.75;
+    // int newPrice = price;
+    // cout<<newPrice<<endl;
+    // return 0;
+
+// input in cpp
+
+    // int age; 
+    // cout<< "Enter your age:" << endl;
+    // cin>>age;
+    // cout<<"your age is : " << age<<endl;
+    // return 0;
+
+}
